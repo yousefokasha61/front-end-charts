681dcfe82ab62e3d7f8a4b9ed4cc37b59cef708b
Youssef Okasha <youssef.okasha@bld.ai>
2022-11-11 20:00:33 +0200

    added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const body = {
+  "id": "7"
+};
 
 function App() {
+
+  const fetchSensorData = () => {
+    console.log("W")
+    axios.post("https://www.splatform.blue:3000/Monthly/BSA", body)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  useEffect(fetchSensorData,[]);
+
   return (
     <div className="App">
       <header className="App-header">
